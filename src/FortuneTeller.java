bc2c1a89855c3e27f84248b4df32db809fb58516
David Byrd <Foolish_Otaku@yahoo.com>
2018-05-05 16:54:47 -0400

    intitial commit

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -1,9 +1,38 @@
+import java.util.Scanner;
 
 public class FortuneTeller {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		Scanner input = new Scanner(System.in);
+		
+		
+		System.out.println("What is your first name?");
+		String name = input.nextLine();
+		System.out.println("Hello," + name);
+		
+		System.out.println("What is your last name");
+		String lastname = input.nextLine();
+		
+		System.out.println("How old are you," + name + "?");
+		int age = 0;
+		input.nextInt();
+		
+		System.out.println("Nice! So " + name + ", what is your favorite ROYGBIV color?");
+		 String color = input.nextLine();
+		 
+		 System.out.println("Awesome!!!, So I have one final question for you" + name + ", how many siblings do you have?");
+		 int sib = 0;
+		 input.nextInt();
+	
+		
+		
+		boolean 
+		
+		
+		
+		
 	}
 
 }
